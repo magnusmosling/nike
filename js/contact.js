ab00df3c1a4a265ffa2d77cd21e65d09c21a1403
Magnus <magnusmosling@live.no>
2022-03-27 19:25:16 +0200

    working on

---
+++ js/contact.js
@@ -0,0 +1,12 @@
+
+const name = document.querySelector("#name");
+const email = document.querySelector("#email");
+const message = document.querySelector("#message");
+const success = document.querySelector("#success");
+const error = document.querySelectorAll(".error");
+
+
+function validateForm(){
+    console.log("hello World");
+
+}
